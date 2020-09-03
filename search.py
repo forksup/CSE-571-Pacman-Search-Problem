44daaf72ded315293c41c78f8bacabaad53544c9
Mitch <31104825+forksup@users.noreply.github.com>
2020-09-02 22:25:11 -0700

    Update search.py

--- search.py
+++ search.py
@@ -65,18 +65,6 @@ class SearchProblem:
         """
         util.raiseNotDefined()
 
-
-def tinyMazeSearch(problem):
-    """
-    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
-    sequence of moves will be incorrect, so only use this for tinyMaze.
-    """
-    from game import Directions
-    s = Directions.SOUTH
-    w = Directions.WEST
-    return [s, s, w, s, w, w, s, w]
-
-
 def DFS(Problem, Start, Visited):
     paths = []
     for state in Problem.getSuccessors(Start):
