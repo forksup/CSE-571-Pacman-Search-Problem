7deb31d8d9a37738fde0a971d9deb9a06226426a
Mitch <31104825+forksup@users.noreply.github.com>
2020-10-06 11:01:02 -0700

    Add files via upload

--- search.py
+++ search.py
@@ -11,13 +11,66 @@
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
+
 """
 In search.py, you will implement generic search algorithms which are called by
 Pacman agents (in searchAgents.py).
 """
-import searchAgents
+
+"""
+
+Question 1:
+
+ Is the exploration order what you would have expected?
+
+    Yes the order is what I expected. Since it is depth first search, pacman takes the first available move when given 
+    several options. The moves are picked in the priority, West, East, South, and North respectively. This is due to
+    the order in the get sucessors function:
+    
+                for direction in [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]:
+
+    Since we are using a stack which is last in first out this direction is reversed. Meaning at any point of conjunction
+    it will take as many west moves as it can until it reaches a conclusion, then began taking east, south, north, etc 
+    up the tree.
+    
+
+Does Pacman actually go to all the explored squares on his way to the goal?
+    
+    No Pacman does not. This is due to the fact the environment is known before game launch and we can calculate the 
+    best path. With less information the pacman agent would have to explore the dead ends.
+
+ Is this the least cost solution
+    
+    No this is not the least cost solution. Depth first search does not always return the most optimal solution,
+    rather just the first solution path it encounters. A better path finding algorithm like A* finds a better path
+    because it considers the cost.
+    
+Question 3:
+    
+ What happens on openMaze for the various search strategies?
+ 
+    Depth first search performs very poorly here. It takes quite a while to find the solution and expands many nodes in
+    the process. Bread first search works much better and finds the solution relatively quickly. UCS performs better
+    by only a small margin, but A* reduces the amount of expanded notes. Below are the counts of expanded nodes:
+    
+        dfs - N/A inf
+        bfs - 683 nodes
+        ucs - 682 nodes
+        A*  - 535 nodes
+        
+Question 7:
+
+ Can you solve mediumSearch in a short time?
+ 
+    No it actually takes quite a while! This is a both a good and bad thing as it means my heuristic is consistent.
+"""
+
 import util
-import copy
+from util import PriorityQueue
+from util import PriorityQueueWithFunction
+from util import Stack
+from copy import deepcopy
+import searchAgents
 
 
 class SearchProblem:
@@ -27,9 +80,6 @@ class SearchProblem:
 
     You do not need to change anything in this class, ever.
     """
-
-    def __init__(self, other):
-        self.problem = other
 
     def getStartState(self):
         """
@@ -65,159 +115,193 @@ class SearchProblem:
         """
         util.raiseNotDefined()
 
-def DFS(Problem, Start, Visited):
-    paths = []
-    for state in Problem.getSuccessors(Start):
-        if (state[0] not in Visited):
-            direction = [state[1]];
-            Visited.append(state[0])
-            result = DFS(Problem, state[0], Visited);
-            if (Problem.isGoalState(state[0])):
-                return direction;
-            if ("" not in result):
-                combined = direction + result
-                paths.append(combined);
-    if (paths):
-        return paths[0];
-    else:
-        return "";
+
+def manhattanHeuristic(state, problem):
+    return util.manhattanDistance(state, problem.goal)
+
+
+def tinyMazeSearch(problem):
+    """
+    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
+    sequence of moves will be incorrect, so only use this for tinyMaze.
+    """
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    return [s, s, w, s, w, w, s, w]
 
 
 def depthFirstSearch(problem):
     """
     Search the deepest nodes in the search tree first.
+
+    Your search algorithm needs to return a list of actions that reaches the
+    goal. Make sure to implement a graph search algorithm.
+
+    To get started, you might want to try some of these simple commands to
+    understand the search problem that is being passed in:
+
+    print "Start:", problem.getStartState()
+    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
-    return DFS(problem, problem.getStartState(), [])
+    return DFS(problem, [[], [problem.getStartState()]])
+
+
+def DFS(problem, item):
+    paths = Stack()
+
+    if problem.isGoalState(item[1][-1]):
+        return item[0]
+
+    for state in problem.getSuccessors(item[1][-1]):
+        if (not state[0] in item[1]):
+            copy = deepcopy(item)
+            copy[0].append(state[1])
+            copy[1].append(state[0])
+            paths.push(copy)
+
+    while not paths.isEmpty():
+        directions = DFS(problem, paths.pop())
+        if (directions):
+            return directions
+
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
-    # Q Format [Node, Parent]
-    if (hasattr(problem,'corners')):
-        pathsFinal = PriorityQueue()
-        corners = problem.corners
-        paths = util.Queue()
-        i = 0
-        for corner in corners:
-            visitedCorners = [corner]
-            problem.goal = corner
-            result = breadExecute(problem,problem.getStartState())
-            paths.push([corner, visitedCorners, result])
-        while(len(paths.list) > 0):
+
+    visited = [problem.getStartState()]
+    parentPath = Stack()
+    parentPath.push([[], problem.getStartState()])
+
+    while not parentPath.isEmpty():
+        paths = parentPath
+        parentPath = Stack()
+
+        while not paths.isEmpty():
+
             item = paths.pop()
-            if(len(item[1]) < 4):
-                for corner in corners:
-                    if(corner not in item[1]):
-                        problem.goal = corner
-                        newItem = copy.deepcopy(item)
-                        newItem[1].append(corner)
-                        newItem[2] = newItem[2] + (breadExecute(problem,newItem[0]))
-                        newItem[0] = corner
-                        paths.push(newItem)
-            else:
-                pathsFinal.push([item[2]],len(item))
-        return pathsFinal.pop()[0]
-    else: return breadExecute(problem,problem.getStartState());
 
-def breadExecute(problem,start):
-    """Search the shallowest nodes in the search tree first."""
-    # Q Format [Node, Parent]
-    Visited = []
-    q = [start]
-    paths = {start: ""}
-    while (len(q) > 0):
-        item = q[0]
-        if problem.isGoalState(item):
-            buildDirection = paths[item]
-            directions = []
-            while len(buildDirection) > 1:
-                directions.append(buildDirection[1])
-                buildDirection = paths[buildDirection[0]]
-            directions.reverse()
-            return directions
-        Visited.append(item);
-        for state in problem.getSuccessors(item):
-            # [Direction, item]
-            if state[0] not in Visited:
-                q.append(state[0])
-                paths[state[0]] = [item, state[1]];
-        q.pop(0)
-    asdf = ""
+            if problem.isGoalState(item[1]):
+                return item[0]
 
-from util import PriorityQueue
+            for state in problem.getSuccessors(item[1]):
+                if not state[0] in visited:
+                    visited.append(state[0])
+
+                    copy = deepcopy(item)
+                    copy[0].append(state[1])
+                    copy[1] = state[0]
+                    parentPath.push(copy)
+
 
 def uniformCostSearch(problem):
-    q = PriorityQueue()
-    q.push([[problem.getStartState()], []], 0)
-    while (not q.isEmpty()):
-        cost = q.heap[0][0]
-        item = q.pop()
-        if (problem.goal in item[0]):
-            return item[1]
-        for state in problem.getSuccessors(item[0][-1]):
-            if (state[0] not in item[0]):
-                newList = copy.deepcopy(item)
-                newList[0].append(state[0])
-                newList[1] = copy.deepcopy(item[1])
-                newList[1].append(state[1])
-                q.push(newList, problem.getCostOfActions(newList[1]))
+    # Used PriorityQueue to easily find lowest cost path
+    frontier = PriorityQueue()
+    visited = []
+
+    # Push the start state with cost 0 to initiate the while loop
+    frontier.push(problem.getStartState(), 0)
+    frontList = [problem.getStartState()]
+
+    paths = {}
+    paths[problem.getStartState()] = []
+
+    while not frontier.isEmpty():
+
+        node = frontier.pop()
+        frontList.remove(node)
+
+        if problem.isGoalState(node):
+            return paths[node]
+
+        visited.append(node)
+
+        for state in problem.getSuccessors(node):
+            # Append move to path history
+            path = deepcopy(paths[node])
+            path.append(state[1])
+
+            if state[0] not in visited and state[0] not in frontList:
+                # Append move to path history
+                paths[state[0]] = path
+
+                # Finally push to the PriorityQueue
+                frontier.push(state[0], problem.getCostOfActions(path))
+                frontList.append(state[0])
+
+            elif state[0] in frontList and problem.getCostOfActions(path) < problem.getCostOfActions(paths[state[0]]):
+                frontier.update(state[0], problem.getCostOfActions(path))
+                paths[state[0]] = path
+
+    # No possible path to goal
     return []
 
-def nullHeuristic(state, problem):
-    return util.manhattanDistance(state, problem.goal)
 
-def ASTAR(problem,start,heuristic=nullHeuristic):
-    q = PriorityQueue()
-    q.push([[start], []], 0)
-    while (not q.isEmpty()):
-        cost = q.heap[0][0]
-        item = q.pop()
-        if (problem.isGoalState(item[0][-1])):
-            return item[1]
-        for state in problem.getSuccessors(item[0][-1]):
-            if (state[0] not in item[0]):
-                newList = copy.deepcopy(item)
-                newList[0].append(state[0])
-                newList[1] = copy.deepcopy(item[1])
-                newList[1].append(state[1])
-                q.push(newList, problem.getCostOfActions(newList[1])+heuristic(state[0],problem))
+def nullHeuristic(state, problem=None):
+    """
+    A heuristic function estimates the cost from the current state to the nearest
+    goal in the provided SearchProblem.  This heuristic is trivial.
+    """
+    return 0
+
 
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
-    if(heuristic == searchAgents.cornersHeuristic):
-        pathsFinal = PriorityQueue()
-        corners = problem.corners
-        paths = util.Queue()
-        i = 0
-        shortestCorner = PriorityQueue()
-        for corner in corners:
-            problem.goal = corner
-            shortestCorner.push(corner, heuristic(problem.getStartState(),problem))
-        corner = shortestCorner.pop()
-        visitedCorners = [corner]
-        problem.goal = corner
-        result = ASTAR(problem,problem.getStartState(),heuristic)
-        paths.push([corner, visitedCorners, result])
-        while(len(paths.list) > 0):
-            item = paths.pop()
-            if(len(item[1]) < 4):
-                shortestCorner = PriorityQueue()
-                for corner in corners:
-                    if(corner not in item[1]):
-                        problem.goal = corner
-                        shortestCorner.push(corner,heuristic(item[0],problem))
-                if(len(shortestCorner.heap) > 0):
-                    corner = shortestCorner.pop()
-                    problem.goal = corner
-                    newItem = copy.deepcopy(item)
-                    newItem[1].append(corner)
-                    newItem[2] = newItem[2] + (ASTAR(problem,newItem[0],heuristic))
-                    newItem[0] = corner
-                    paths.push(newItem)
-            else:
-                pathsFinal.push([item[2]],len(item))
-        return pathsFinal.pop()[0]
-    else:
-        return ASTAR(problem,problem.getStartState(),heuristic)
+    return ASTAR(problem, problem.getStartState(), heuristic)
+
+
+def ASTAR(problem, start, heuristic):
+    # Used PriorityQueue to easily find lowest cost path
+    frontier = PriorityQueue()
+    visited = []
+
+    paths = {}
+    paths[generateIndex(start)] = []
+
+    frontier.push(start, 0)
+    while not frontier.isEmpty():
+
+        node = frontier.pop()
+        path = paths[generateIndex(node)]
+
+        if problem.isGoalState(node):
+            return path
+
+        visited.append(node)
+
+        for state in problem.getSuccessors(node):
+            # Append move to path history
+            newPath = deepcopy(path)
+            newPath.append(state[1])
+
+            membership = False
+
+            i = 0
+            for item in frontier.heap:
+                if item[2] == state[0]:
+                    membership = True
+                    break
+                i += 1
+
+            if state[0] not in visited and not membership:
+                # Append move to path history
+
+                # Finally push to the PriorityQueue
+                frontier.push(state[0], problem.getCostOfActions(newPath) + heuristic(state[0], problem))
+                paths[generateIndex(state[0])] = newPath
+            elif membership:
+                if (problem.getCostOfActions(newPath) + heuristic(state[0], problem)) < frontier.heap[i][0]:
+                    frontier.update(state[0], problem.getCostOfActions(newPath) + heuristic(state[0], problem))
+                    paths[generateIndex(state[0])] = newPath
+
+    # No possible path to goal
+    return []
+
+
+def generateIndex(array):
+    return hash(str(array))
+    return hash(array)
 
 
 # Abbreviations
